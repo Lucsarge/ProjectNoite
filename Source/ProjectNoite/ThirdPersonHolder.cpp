9fd00de5741a4794580b688a7c8e119ec3df2130
Luc <Lucsarge@gmail.com>
2020-08-16 21:35:32 -0400

    Added Spawn Gates and basedrones with limited functionality

---
+++ Source/ProjectNoite/ThirdPersonHolder.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ThirdPersonHolder.h"
+
+ThirdPersonHolder::ThirdPersonHolder()
+{
+}
+
+ThirdPersonHolder::~ThirdPersonHolder()
+{
+}
